46f7b9984ccd96df8ad4ca46f9efbfa2747ba4dc
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-12-24 01:17:38 +0500

    Добавьте файлы проекта.

---
+++ LLM_GigaPicture_11_02_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLM_GigaPicture_11_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
